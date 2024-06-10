56b18455f4aed119ccadf4e5b11db59730339388
JuanchoZGZ17 <756640@unizar.es>
2024-06-10 18:09:04 +0200

    Gateway

---
+++ gateway.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+// Middleware para registrar todas las solicitudes
+app.use((req, res, next) => {
+    console.log(`Request received: ${req.method} ${req.url}`);
+    next();
+});
+
+// Configurar el proxy para redirigir todas las solicitudes a http://localhost:4040
+app.use('/', createProxyMiddleware({
+    target: 'http://localhost:3000',
+    changeOrigin: true,
+}));
+
+// Iniciar el servidor en el puerto 4000
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Gateway running on port ${PORT}`);
+});
